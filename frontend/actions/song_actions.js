391fcfe84921f99740eca9f0e84aa04d39b267ec
ranellereyes <ranelle.reyes@gmail.com>
2017-05-18 12:02:57 -0700

    Add Song API Utils/AJAX Requests

---
+++ frontend/actions/song_actions.js
@@ -0,0 +1,26 @@
+// API UTIL
+import * as APIUtil from '../util/song_util';
+
+// CONSTANTS
+
+// EXAMPLES
+// export const LOGIN = "LOGIN";
+
+// ASYNC ACTIONS
+// EXAMPLES
+
+// export const actionSignup = user => dispatch => {
+//   return APIUtil.signupRequest(user).then(
+//     currentUser => dispatch(receiveCurrentUser(currentUser)),
+//     error => {
+//       return dispatch(receiveErrors(error.responseJSON));}
+//   );
+// };
+
+// // SYNC ACTIONS
+// EXAMPLES
+
+// export const receiveCurrentUser = currentUser => ({
+//   type: RECEIVE_CURRENT_USER,
+//   currentUser
+// });
---
+++ frontend/util/song_util.js
@@ -0,0 +1,37 @@
+export const songsRequest = () => (
+  $.ajax({
+    method: 'GET',
+    url: '/api/songs'
+  })
+);
+
+export const newSongRequest = (song) => (
+  $.ajax({
+    method: 'POST',
+    url: `/api/songs/`,
+    data: {song}
+  })
+);
+
+export const songRequest = (song) => (
+  $.ajax({
+    method: 'GET',
+    url: `/api/song/${song.id}`,
+    data: {song}
+  })
+);
+
+export const editSongRequest = (song) => (
+  $.ajax({
+    method: 'PATCH',
+    url: `/api/song/${song.id}`,
+    data: {song}
+  })
+);
+
+export const deleteSongRequest = (song) => (
+  $.ajax({
+    method: 'DELETE',
+    url: `/api/song/${song.id}`
+  })
+);
