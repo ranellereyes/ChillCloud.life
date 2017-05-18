d47f07db28989944923f0ebe5aabea7801ad1629
ranellereyes <ranelle.reyes@gmail.com>
2017-05-17 17:26:28 -0700

    Change background image to work with heroku

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Splash = () => (
   <div className="splash-image">
-    <img src="assets/splash_image.jpeg" />
     <div className="splash-text">
       <h1 className="title">ChillCloud</h1>
       <p className="title">listen & chill</p>
