eb67fe634b0c1fc8a0a2fb13f9f336bc6d740491
ranellereyes <ranelle.reyes@gmail.com>
2017-05-23 15:35:42 -0700

    Added skip button

--- frontend/components/audio_player/audio_player.jsx
+++ frontend/components/audio_player/audio_player.jsx
@@ -31,6 +31,9 @@ class AudioPlayer extends React.Component {
           src={playlist[0].img}
           className="small-player"/>
         <div>
+          <img
+            src="https://image.flaticon.com/icons/svg/149/149660.svg"
+            onClick={this.props.next} />
           <audio
             src={playlist[0].src}
             id="Audio"
