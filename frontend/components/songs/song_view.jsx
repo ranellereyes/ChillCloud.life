0ccc3a51829093a8c254721d0268c400566d1799
ranellereyes <ranelle.reyes@gmail.com>
2017-05-23 21:22:22 -0700

    Expanded show-page and start of comments

--- frontend/components/songs/song_view.jsx
+++ frontend/components/songs/song_view.jsx
@@ -32,12 +32,15 @@ class SongView extends React.Component {
     super(props);
 
     this.state = {
-      isOpen: false
+      isOpen: false,
+      rand: `https://s3-us-west-1.amazonaws.com/chillcloud-dev/stock/cover${Math.ceil(8 * Math.random())}.jpg`
     };
 
     this.openForm = this.openForm.bind(this);
     this.closeForm = this.closeForm.bind(this);
     this.delete = this.delete.bind(this);
+    this.playSong = this.playSong.bind(this);
+    this.queueSong = this.queueSong.bind(this);
     // this.props.getSong = this.props.getSong.bind(this);
   }
 
@@ -54,7 +57,16 @@ class SongView extends React.Component {
   }
 
   componentDidMount() {
+    this.setState({rand: `https://s3-us-west-1.amazonaws.com/chillcloud-dev/stock/cover${Math.ceil(8 * Math.random())}.jpg`});
     this.props.getSong(this.props.match.params.song_id);
+  }
+
+  playSong () {
+    this.props.play(this.props.songs);
+  }
+
+  queueSong () {
+    this.props.queue(this.props.songs);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -66,7 +78,6 @@ class SongView extends React.Component {
       this.props.getSong(this.props.match.params.song_id);
     }
   }
-
 
   componentDidUpdate() {
     if (this.props.songs.redirect === "stream") {
@@ -83,7 +94,7 @@ class SongView extends React.Component {
   render () {
     const {songs, currentUser} = this.props;
     const buttons = (
-      <div>
+      <div className="artist-buttons">
         <button onClick={this.openForm}>
           <span>
             Update!
@@ -97,14 +108,35 @@ class SongView extends React.Component {
       </div>
     );
 
+    let randStyle = {
+      backgroundImage: `url(${this.state.rand})`
+    };
+
     return Object.keys(songs).length !== 0 ? (
       <main className="song-view-main">
-        <div className="song-view">
-          <SongItem
-            song={songs}
-            currentUser={currentUser}
-            queue={this.props.queue}
-            play={this.props.play} />
+        <div
+          className="song-view"
+          style={randStyle}>
+          <img src={songs.image_url} />
+          <div className="second-col">
+            <section>
+              <div className="buttons">
+                <button
+                  className="play-button"
+                  onClick={this.playSong}
+                  />
+                <button
+                  className="queue-button"
+                  onClick={this.queueSong}>+</button>
+              </div>
+              <ul className="details">
+                <li key={`title-${songs.id}`}>{songs.title}</li>
+                <li key={`artist-${songs.id}`}>{songs.artist}</li>
+              </ul>
+            </section>
+            {currentUser.id === songs.user_id ? buttons : null}
+          </div>
+            <p className="genre">{songs.genre ? `#${songs.genre}` : null}</p>
         </div>
         <Modal
           isOpen={this.state.isOpen}
@@ -116,10 +148,18 @@ class SongView extends React.Component {
             closeModal={this.closeForm}
             type="update" />
         </Modal>
-        {currentUser.id === songs.user_id ? buttons : null}
-        <ul>
-          Comments!
-        </ul>
+        <div className="bottom-view">
+          <ul className="comments">
+            <input
+              type="text">
+            </input>
+              Comments!
+          </ul>
+          <ul
+            className="artist-similar">
+            Similar songs from this artist!
+          </ul>
+        </div>
       </main>
     ) : null;
   }
